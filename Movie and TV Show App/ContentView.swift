fd2fc7e8d8203b3892721de5b27695f5efe19400
Shravan Prasanth <shra.prasanth@gmail.com>
2021-07-28 15:01:22 -0400

    Initial Commit

---
+++ Movie and TV Show App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie and TV Show App
+//
+//  Created by Shravan Prasanth on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie and TV Show App
+
+## This app reccommends movies based on what the user has watched, and users are able to keep track of upcoming movies. 
